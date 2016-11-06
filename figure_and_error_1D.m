cb13498771ebc663f585e5fe238cc52bd2abf387
Talvd <talvdr@gmail.com>
2016-11-06 11:03:15 +0200

    Update figure_and_error_1D.m

--- figure_and_error_1D.m
+++ figure_and_error_1D.m
@@ -1,4 +1,4 @@
-function figure_and_error_1D(points,new_points,f, f_new_org, f_new, figure_title, display_figure, more_figures)
+function [h] = figure_and_error_1D(points,new_points,f, f_new_org, f_new, in_figure_title, display_figure, more_figures, h)
 
 % err_norm = norm(f_new_org - f_new);
 rel_norm = norm(f_new_org - f_new)/norm(f_new_org);
@@ -8,7 +8,7 @@ if (strcmp(err_str, '0.0000'))
     err_str = num2str(rel_norm,'%10.4e');
 end
 
-figure_title = [figure_title ' relative error ' err_str];
+figure_title = [in_figure_title ' relative error ' err_str];
 
 fprintf([figure_title '\n']);
 
@@ -25,9 +25,20 @@ if (display_figure)
 %         plot(new_points,err);
 %         legend('ERROR');
         rel_err = abs((f_new_org - f_new)./f_new_org);
-        figure
-        plot(new_points,rel_err);
-        legend('Relative ERROR');
+        if nargin<9
+            figure
+            plot(new_points,rel_err);
+        else
+            figure(h)
+            hold all
+            p = plot(new_points,rel_err);
+            set(p, 'LineWidth',2);
+            ind =  strfind(in_figure_title, 'function');
+            fig_legend = in_figure_title(1:ind-1);
+            [~,~,~,text]=legend;
+            legend(text,fig_legend)            
+        end               
+        title('Relative ERROR');
         figure
         d = diff(f')./diff(points);
         plot(points(1:(end-1)),d);
